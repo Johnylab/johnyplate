2b345b0191305e2be06c9923d22cabd72c37c6d0
Johnylab <joaobatistacomunic@hotmail.com>
2018-02-06 15:49:05 -0200

    Autoprefixer automático com (npm install gulp-autoprefixer)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var cp          = require('child_process');
-var rollup      = require('gulp-rollup');
-var babel       = require('gulp-babel');
-var sourcemaps  = require('gulp-sourcemaps');
+var cp = require('child_process');
+var rollup = require('gulp-rollup');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
@@ -19,11 +20,22 @@ var browserSyncRoutes = {
 
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+    return cp.spawn(jekyll, ['build'], {
+            stdio: 'inherit'
+        })
         .on('close', done);
 });
 
-gulp.task('bundle', function() {
+gulp.task('post-css', function () {
+    gulp.src('_site/css/main.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('_site/css/'));
+});
+
+gulp.task('bundle', function () {
     gulp.src('./_scripts/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(rollup({
@@ -35,15 +47,14 @@ gulp.task('bundle', function() {
             "presets": ["es2015"]
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./js'))
-        ;
-  });
+        .pipe(gulp.dest('./js'));
+});
 
 gulp.task('jekyll-rebuild', ['bundle', 'jekyll-build'], function () {
     browserSync.reload();
 });
 
-gulp.task('browser-sync', ['bundle', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['bundle', 'jekyll-build'], function () {
     browserSync({
         server: {
             baseDir: '_site',
@@ -54,15 +65,18 @@ gulp.task('browser-sync', ['bundle', 'jekyll-build'], function() {
 
 gulp.task('watch', function () {
     gulp.watch([
-        './*', 
-        '_layouts/**/*', 
-        '_includes/**/*', 
-        '_sections/**/*', 
-        '_sass/**/*', 
-        'css/**/*', 
-        '_scripts/**/*', 
+        './*',
+        '_layouts/**/*',
+        '_includes/**/*',
+        '_sections/**/*',
+        '_sass/**/*',
+        'css/**/*',
+        '_scripts/**/*',
         'js/**/*'
     ], ['jekyll-rebuild']);
+    gulp.watch([
+        '_site/css/**/*'
+    ], ['post-css']);
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
